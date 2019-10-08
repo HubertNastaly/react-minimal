1c4a2c4c2238bf0b873d78ab907c160c6c4009f6
HubertNastaly <h.nastaly@gmail.com>
2019-10-08 11:32:17 +0200

    Lab1Ex1

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const AppTitle = () => (
-    <p>Bundle size: 128950 bytes, Load time of the bundle: 50 ms, Last commit SHA1: 30992b7ca32cb7e9d39a6b364cfb3eb4bea58631</p>
+  <p>Bundle size: 128950 bytes, Load time of the bundle: 50 ms, Last commit SHA1: 30992b7ca32cb7e9d39a6b364cfb3eb4bea58631</p>
 )
 
 export default AppTitle
