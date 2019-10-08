a56da58fcaefa1ab52f082e33e9f2b06be746683
HubertNastaly <h.nastaly@gmail.com>
2019-10-08 11:16:33 +0200

    Lab1Ex1

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const App = () => (
-  <h1>React Minimal nastalyh 2fe2de23beaa6a66026d2915e8bae0543275df14
-    <p>Bundle size: 128950 bytes, Load time of the bundle: 50 ms, Last commit SHA1: 30992b7ca32cb7e9d39a6b364cfb3eb4bea58631</p>
-  </h1>  
+  <h1></h1>  
 )
 
 export default App
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './MyApp'
+import AppTitle from './AppTitle'
 
-render(<App />, document.getElementById('reactinjectedhere'))
+render(<App />, document.getElementById('reactinjectedhere'));
+render(<AppTitle/>, document.getElementsByTagName('h1').item(0));
