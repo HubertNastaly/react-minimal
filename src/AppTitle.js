77110603b6705c1bd24d0f4481398453a9a9a7d1
HubertNastaly <h.nastaly@gmail.com>
2019-10-09 21:23:39 +0200

    Lab1Ex4

--- src/AppTitle.js
+++ src/AppTitle.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AppTitle = () => (
   <p>
     Bundle size: 128950 bytes, Load time of the bundle: 50 ms, Last commit SHA1: 30992b7ca32cb7e9d39a6b364cfb3eb4bea58631
-    <script>{console.log(generateRandomArray(7))}</script>
+    <script>{console.log(largerThan15())}</script>
   </p>  
 )
 
@@ -11,4 +11,6 @@ export default AppTitle
 
 const generateArray = (n) => Array.from(new Array(n), (val, idx) => idx + 1);
 const generateRandomArray = (n) => Array.from(new Array(n), (val) => val = Math.round(Math.random() * (25 - 1)) + 1);
+const constArray = [2, 56, 23, 88, 17, 4];
+const largerThan15 = () => constArray.filter((val) => val > 15);
 
